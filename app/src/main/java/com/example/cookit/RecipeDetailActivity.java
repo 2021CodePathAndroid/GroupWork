3547828a83dc3ae6652cfcbcab24f27cd7bcd72a
Xaktor <71800595+Xaktor@users.noreply.github.com>
2021-04-21 20:56:59 -0400

    Creating branch

---
+++ app/src/main/java/com/example/cookit/RecipeDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.cookit.models;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cookit.R;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_detail);
+    }
+}

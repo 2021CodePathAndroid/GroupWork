e83574e463399891e6cfa3b80d2f15d5dc3cf328
Xaktor <71800595+Xaktor@users.noreply.github.com>
2021-04-29 21:01:36 -0400

    commiting

--- app/src/main/java/com/example/cookit/RecipeDetailActivity.java
+++ app/src/main/java/com/example/cookit/RecipeDetailActivity.java
@@ -1,16 +1,21 @@
-package com.example.cookit.models;
+package com.example.cookit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.cookit.R;
 
 public class RecipeDetailActivity extends AppCompatActivity {
-
+    TextView tvTitle;
+    TextView tvDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_detail);
+        tvTitle=findViewById(R.id.tvTitle);
+        tvDetails=findViewById(R.id.tvDetails);
+
     }
 }
